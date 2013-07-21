95dc5baa2eeb09bcc27e731c21d0b7f9472b5abd
Dmitry Protopopov <dmitry.protopopov@gmail.com>
2013-07-21 22:49:44 +0400

    1

---
+++ js/phonespell.js
@@ -0,0 +1,88 @@
+
+function addPhoneItem(displayName,phoneNumber) {
+	var item = $(".item-template").clone();
+	item.appendTo(".items").removeClass("item-template").addClass("item"); 
+	item.find("[data-role='none']").removeAttr("data-role");
+	item.find("#item-title").text(displayName);
+	item.find("#item-count").text(phoneNumber);
+	item.find("a").jqmData("phoneNumber",phoneNumber);
+	item.find("a").bind("vclick",function(event) {
+		if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
+		var phoneNumber = $(this).jqmData("phoneNumber");
+		var myPhoneSpellForm = $("#myPhoneSpellForm");
+		var phoneInput = $("#phone", myPhoneSpellForm);
+		phoneInput.val(phoneNumber);
+		$.mobile.changePage("#myPhoneSpell");
+		return false;
+	});
+}
+
+var deviceReadyDeferred = $.Deferred();
+var jqmReadyDeferred = $.Deferred();
+
+$.when(deviceReadyDeferred, jqmReadyDeferred).then(function() {
+	debugWrite('when(deviceReadyDeferred, jqmReadyDeferred).then','start');
+	var contactFields = ["displayName", "phoneNumbers"];
+	var contactSuccess = function (contacts) {
+		for (var i=0; i<contacts.length; i++) {
+			debugWrite("contacts",contacts[i]);
+			if (contacts[i].phoneNumbers){
+				for(var j=0; j<contacts[i].phoneNumbers.length; j++) {
+					var item = addPhoneItem(contacts[i].displayName,contacts[i].phoneNumbers[j].value);
+				}
+			}
+		}
+		$("#phoneChoose .items.ui-listview").listview("refresh");		
+	}
+	var contactError = function(error) {
+		// Handle the error
+		debugWrite('contactError',error);
+		navigator.notification.alert(contactErrorMessage(error)+'(Error code: ' + error.code +')', null, 'Contact Error');
+		return false;
+	}
+	var contactFindOptions = {
+		filter: "",
+		multiple: true
+	};
+	navigator.contacts.find(contactFields, contactSuccess, contactError, contactFindOptions);
+	debugWrite('when(deviceReadyDeferred, jqmReadyDeferred).then','end');
+});
+
+$(document).one("pageinit",function(event) {
+	debugWrite("pageinit","one");
+	jqmReadyDeferred.resolve();
+	
+	var myPhoneSpellForm = $("#myPhoneSpellForm");
+	
+    myPhoneSpellForm.validate();
+	
+	myPhoneSpellForm.submit(function(event) {
+		if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
+        if ($(this).valid()) {
+			var myPhoneSpellResult = $("#myPhoneSpellResult");
+			$(this).ajaxSubmit({
+				target:myPhoneSpellResult
+			});
+		}
+		return false;
+    });
+});
+
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("backbutton", handleBackButton, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
+	debugWrite("onDeviceReady","start");
+	deviceReadyDeferred.resolve();
+	debugWrite("onDeviceReady","end");
+}
+
+function handleBackButton() {
+  	debugWrite("Back Button Pressed!","start");
+    navigator.app.exitApp();
+  	debugWrite("Back Button Pressed!","end");
+}
---
+++ js/snippets.js
@@ -0,0 +1,75 @@
+function loadSettings() {
+	try {
+		$("#settings input[type='text'],#settings input[type='password']").each(function() {
+			$(this).val(window.localStorage.getItem($(this).attr("id")));
+		});
+	} catch(e) {
+		debugWrite("error",e);
+	}
+}
+function saveSettings() {
+	try {
+		$("#settings input[type='text'],#settings input[type='password']").each(function() {
+			window.localStorage.setItem($(this).attr("id"),$(this).val());
+		});
+	} catch(e) {
+		debugWrite("error",e);
+	}
+}
+function loadImage(image, imagePath) {
+	debugWrite("loadImage",imagePath);
+	var createReader = function (readable) {
+		debugWrite("createReader","start");
+		var reader = new FileReader();
+		reader.onloadend = function (evt) {
+			debugWrite("reader.onloadend","start");
+			var dataURL = evt.target.result;
+			$(image).attr("src",dataURL);
+			debugWrite("reader.onloadend","end");
+		}
+		reader.readAsDataURL(readable);
+		debugWrite("createReader","end");
+	};    
+	var gotFileEntry = function (fileEntry) {
+		debugWrite("gotFileEntry","start");
+		fileEntry.file(createReader, fail);
+		debugWrite("gotFileEntry","end");
+	};
+	window.resolveLocalFileSystemURI(imagePath,gotFileEntry, fail);
+}
+
+// Функция вывода сообщений трассировки
+// Обработка try-catch требуется для совместимости с IE
+function debugWrite(a,b) {
+	try {
+		console.log(a,b);
+	} catch (e) {
+	}
+}
+
+function parseDate(input) {
+  var parts = input.split('-');
+  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+}
+
+function parseDatetime(input) {
+  var parts = input.split("T");
+  var dateParts = parts[0].split('-');
+  var timeParts = parts[1].split(':');
+  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(dateParts[0], dateParts[1]-1, dateParts[2], timeParts[0], timeParts[1]); // months are 0-based
+}
+
+function contactErrorMessage(error) {
+	switch(error.code) {
+		case ContactError.UNKNOWN_ERROR: return "UNKNOWN_ERROR"; break;
+		case ContactError.INVALID_ARGUMENT_ERROR: return "INVALID_ARGUMENT_ERROR"; break;
+		case ContactError.TIMEOUT_ERROR: return "TIMEOUT_ERROR"; break;
+		case ContactError.PENDING_OPERATION_ERROR: return "PENDING_OPERATION_ERROR"; break;
+		case ContactError.IO_ERROR: return "IO_ERROR"; break;
+		case ContactError.NOT_SUPPORTED_ERROR: return "NOT_SUPPORTED_ERROR"; break;
+		case ContactError.PERMISSION_DENIED_ERROR: return "PERMISSION_DENIED_ERROR"; break;
+	}
+	return "";
+}
